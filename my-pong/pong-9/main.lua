a1fd266079eb5c476dc3d2cdf4fb1cbad3f26925
Gustavo Pinheiro <gustavospr@hotmail.com>
2022-11-01 16:32:42 -0300

    Add serve state and player turn

--- my-pong/pong-9/main.lua
+++ my-pong/pong-9/main.lua
@@ -34,6 +34,8 @@ function love.load()
     player1Score = 0
     player2Score = 0
 
+    servingPlayer = 1
+
     player1 = Paddle(10, 30, 5, 20)
     player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
 
@@ -43,34 +45,36 @@ function love.load()
 end
 
 function love.update(dt)
-    if gameState == 'play' then
+    if gameState == 'serve' then
+        ball.dy = math.random(-50, 50)
+        if servingPlayer == 1 then
+            ball.dx = math.random(140, 200)
+        else
+            ball.dx = math.random(140, 200)
+        end
+    elseif gameState == 'play' then
         if ball:collides(player1) then
             ball.dx = -ball.dx * 1.03
             ball.x = player1.x + 5
-
             if ball.dy < 0 then
                 ball.dy = -math.random(10, 150)
             else
                 ball.dy = math.random(10, 150)
             end
         end
-
         if ball:collides(player2) then
             ball.dx = -ball.dx * 1.03
             ball.x = player2.x - 4
-
             if ball.dy < 0 then
                 ball.dy = -math.random(10, 150)
             else
                 ball.dy = math.random(10, 150)
             end
         end
-        
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
         end
-
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
@@ -81,14 +85,14 @@ function love.update(dt)
         servingPlayer = 1
         player2Score = player2Score + 1
         ball:reset()
-        gameState = 'start'
+        gameState = 'serve'
     end
 
     if ball.x > VIRTUAL_WIDTH then
         servingPlayer = 2
         player1Score = player1Score + 1
         ball:reset()
-        gameState = 'start'
+        gameState = 'serve'
     end
 
     if love.keyboard.isDown('w') then
@@ -121,11 +125,9 @@ function love.keypressed(key)
 
     elseif key == 'enter' or key == 'return' then
         if gameState == 'start' then
+            gameState = 'serve'
+        elseif gameState == 'serve' then
             gameState = 'play'
-        else
-            gameState = 'start'
-
-            ball:reset()
         end
     end
 end
@@ -137,34 +139,21 @@ function love.draw()
 
     love.graphics.setFont(smallFont)
     if gameState == 'start' then
+        love.graphics.printf('Welcome to Pong!', 0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Press Enter to begin!', 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'serve' then
         love.graphics.printf(
-            'Hello Start State!',
+            'Player ' .. tostring(servingPlayer) .. "'s serve!",
             0,
-            20,
+            10,
             VIRTUAL_WIDTH,
             'center'
         )
-    else
-        love.graphics.printf(
-            'Hello Play State!',
-            0,
-            20,
-            VIRTUAL_WIDTH,
-            'center'
-        )
+        love.graphics.printf('Press Enter to serve!', 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'play' then
     end
 
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(
-        tostring(player1Score),
-        VIRTUAL_WIDTH / 2 - 50,
-        VIRTUAL_HEIGHT / 3
-    )
-    love.graphics.print(
-        tostring(player2Score),
-        VIRTUAL_WIDTH / 2 + 40,
-        VIRTUAL_HEIGHT / 3
-    )
+    displayScore()
 
     player1:render()
 
@@ -181,4 +170,18 @@ function displayFPS()
     love.graphics.setFont(smallFont)
     love.graphics.setColor(0, 255/255, 0, 255/255)
     love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
+end
+
+function displayScore()
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(
+        tostring(player1Score),
+        VIRTUAL_WIDTH / 2 - 50,
+        VIRTUAL_HEIGHT / 3
+    )
+    love.graphics.print(
+        tostring(player2Score),
+        VIRTUAL_WIDTH / 2 + 40,
+        VIRTUAL_HEIGHT / 3
+    )
 end
